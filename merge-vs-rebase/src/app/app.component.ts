163aefdb2e7f8b38dfea26be6075c316fb645f31
anjaliTandel20 <50238696+anjaliTandel20@users.noreply.github.com>
2023-08-10 12:50:17 +0530

    Feature Commit 1

--- merge-vs-rebase/src/app/app.component.ts
+++ merge-vs-rebase/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'merge-vs-rebase';
+  public title: string = 'merge-vs-rebase';
 }
