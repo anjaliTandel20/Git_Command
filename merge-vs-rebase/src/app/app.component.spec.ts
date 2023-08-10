f905d5ad3100d5bcb051bc597b48ed4fb9d81c4f
anjaliTandel20 <50238696+anjaliTandel20@users.noreply.github.com>
2023-08-10 12:59:11 +0530

    Feature Commit 3

--- merge-vs-rebase/src/app/app.component.spec.ts
+++ merge-vs-rebase/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+// Test cases
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     declarations: [AppComponent]
